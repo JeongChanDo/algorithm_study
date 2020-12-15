fa7f9534b56f99f5e87fa67cfa2014dcc73d77c9
JeongChanDo <djc4223@naver.com>
2020-12-15 21:20:54 +0900

    add selection sort

--- 01_introduction/binary_search.py
+++ 01_introduction/binary_search.py
@@ -1,9 +1,10 @@
+import math
 def binary_search(lst, item):
     low = 0
     high = len(lst) - 1
 
     while low <= high:
-        mid = (low + high)
+        mid = math.floor((low + high)/2)
         guess = lst[mid]
         if guess == item:
             return mid
@@ -14,11 +15,9 @@ def binary_search(lst, item):
     return None
 
 def main():
-    my_list = [1, 3, 5, 7, 9]
-    print(binary_search(my_list, 3))
+    my_list = [1, 3, 5, 7, 9, 21]
+    print(binary_search(my_list, 9))
     print(binary_search(my_list, -1))
-
-
 
 if __name__ == "__main__":
     main()
